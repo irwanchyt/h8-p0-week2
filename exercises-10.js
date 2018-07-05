04e17c902c8ffc8ac21d5891740e67058eb5fba9
irwancahyt <sdirwancahyanto@gmail.com>
2018-07-05 22:14:00 +0700

    latihan 10

---
+++ exercises-10.js
@@ -0,0 +1,20 @@
+/*jika num2 > num1 = true
+  jika num2 < num1 = false
+  jika num1 = num2 = -1
+   */
+function bandingkanAngka(num1,num2){
+  var num1 ;
+  var num2 ;
+if (num2 > num1){
+console.log('true');
+
+}
+else if(num2 < num1) {
+  console.log('false');
+}
+else if (num1 = num2){
+  console.log('-1');
+return;
+}
+}
+bandingkanAngka(4,4)
