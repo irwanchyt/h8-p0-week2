231857ab1e801a64031370833ad85d8ebb9ab0a8
irwancahyt <sdirwancahyanto@gmail.com>
2018-07-05 21:20:54 +0700

    tugas function

---
+++ exercises-9.js
@@ -0,0 +1,36 @@
+/soal 1/
+function shoutOut (){
+
+console.log('Hallo Function!');
+return;
+}
+
+shoutOut();
+
+/soal 2/
+function calculateMultiply(){
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = (num1*num2);
+return(hasilPerkalian);
+
+}
+
+calculateMultiply()
+
+/soal 3/
+function processSentence(){
+var name = 'Agus';
+var age = 30;
+var address = 'Jln. Malioboro, Yogjakarta';
+var hobby = 'gaming';
+
+var fullSentence = (name,age,address,hobby);
+console.log('Nama saya '+ name + ' umur saya ' + age +' tahun , alamat saya di '+ address +'dan saya punya hobby yaitu '+ hobby + '!');
+
+return (fullSentence);
+
+
+}
+processSentence();
