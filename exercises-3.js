12aa640100d0fa3e300e5425b9a2f625ecc7b377
irwancahyt <sdirwancahyanto@gmail.com>
2018-07-06 17:10:16 +0700

    if else terbaru

--- exercises-3.js
+++ exercises-3.js
@@ -1,52 +1,35 @@
-var nama = '';
-var peran ='';
+var nama = ''
+var peran= ''
 
-if (nama ==='' && peran ==='' ){
-  console.log('kamu harus mengisi nama dulu!');
-}
-  else if(nama ==='irwan'&& peran===''){
-    console.log('Hallo irwan,,, pilih peran mu sekarang! ');
+if (nama ===''){
 
-  }
-  else if (nama ==='dwi'&& peran ==='batman'){
-  console.log('Selamat datang di Dunia Proxytia, dwi');
-  console.log('kamu telah memilih peran menjadi batman ,ya');
-  }
-  else if (nama ==='sulis' && peran ==='superman'){
-  console.log('Selamat datang di Dunia Proxytia, sulis');
-  console.log('kamu telah memilih peran menjadi superman ,ya');
-  }
-  else if (nama ==='hafiz'|| peran ==='gatot kaca'){
-  console.log('Selamat datang di Dunia Proxytia, hafiz');
-  console.log('kamu telah memilih peran menjadi gatot kaca,ya');
+  console.log('Nama harus diisi!')
+}
 
-  }
-  else if (nama ===''&& peran ==='batman'){
-    console.log('kamu belum mememasukan nama');
-  }
-   else if (nama ===''&& peran ==='superman'){
-    console.log('kamu belum memasukan nama');
-  }
-   else if (nama ===''&& peran ==='gatotkaca'){
-    console.log('kamu belum memasukan nama');
-  }
-  else if (nama !==''&& peran ==='batman'){
-    console.log(' hallo kamu sudah memilih peran mu ya sebagai batman ya');
-}
-else if (nama !==''&& peran ==='superman'){
-    console.log(' hallo kamu sudah memilih peran mu ya sebagai superman ya');
-}
-else if (nama !==''&& peran ==='gatotkaca'){
-    console.log(' hallo kamu sudah memilih peran mu ya sebagai gatotkaca ya');
-}
-else if(nama !== '' && peran ===''){
-  console.log('hallo kamu belum memilih peran ya');
+else if(peran=== '' ){
 
+  console.log('Halo '+ nama +' , Pilih peranmu untuk memulai game!')
 }
-else if(nama !=='' && peran !== '' ){
-console.log('haaii,hanya ada 3 peran disini yaitu Batman ,Superman, gatotkaca ');
+else if (peran ==='ksatria'){
+
+  console.log('Selamat datang di Dunia Proxytia,'+ nama )
+
+  console.log('Halo Ksatria '+ nama +' kamu dapat menyerang dengan senjatamu!')
 }
-else if(nama ==='' && peran !== '' ){
-console.log('haaii,hanya ada 3 peran disini yaitu Batman ,Superman, gatotkaca ');
-console.log('jangan lupa masukan nama ya');
+
+else if (peran ==='tabib'){
+
+  console.log('Selamat datang di Dunia Proxytia,'+ nama )
+
+  console.log('Halo Tabib ' + nama +' kamu akan membantu temanmu yang terluka.')
+}
+else if (peran ==='penyihir'){
+
+  console.log('Selamat datang di Dunia Proxytia,'+ nama )
+
+  console.log('Halo penyihir ' + nama +', ciptakan keajaiban yang membantu kemenanganmu!')
+}
+else{
+
+  console.log('pilih peran mu sebagai ksatria, tabib dan penyihir')
 }
