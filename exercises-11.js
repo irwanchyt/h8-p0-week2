f0061cd4824ce3afb642324f141ab56440c648d2
irwancahyt <sdirwancahyanto@gmail.com>
2018-07-07 22:18:10 +0700

    balikKata

---
+++ exercises-11.js
@@ -0,0 +1,13 @@
+function balikKata(kata){
+
+ var kataBaru = ''
+ var sum = kata.length-1
+
+for ( var i = sum ; i >=0 ; i--){
+ kataBaru += kata[i]
+ //console.log(kataBaru)
+
+}
+//console.log(kataBaru)
+return(kataBaru)
+}
